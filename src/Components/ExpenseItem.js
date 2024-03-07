61b6677f19f1169029d00de68b868de820b37c3c
Sugyan98 <sugyanmahakud@gmail.com>
2024-03-07 23:57:30 +0530

    styling

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,17 @@
+import './ExpenseItem.css'
+
 function ExpenseItem (){
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
 
     return (
-        <div>
-            <h2>Expense item!</h2>
-            <li>Food Rs 10</li>
-            <li>Petrol Rs 100</li>
-            <li>Movies Rs 200</li>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__desciption">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
         </div>
     )
 
